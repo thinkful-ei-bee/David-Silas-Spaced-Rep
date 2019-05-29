c0cf7867f530901224e39eb6056e24935e63098a
Loxphordex <silasishallahan@gmail.com>
2019-05-29 09:22:48 -0700

    still working on display score

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -5,9 +5,13 @@ import LanguageService from '../../services/language-service'
 class LearningRoute extends Component {
   state = {
     nextWord: '',
+    answer: null,
+    lastWord: '',
     wordCorrectCount: null,
     wordIncorrectCount: null,
     totalScore: null,
+    guess: '',
+    correctGuess: 0,
   }
 
   componentDidMount() {
@@ -15,6 +19,7 @@ class LearningRoute extends Component {
       .then(head => {
         this.setState({
           nextWord: head.nextWord,
+          lastWord: head.nextWord,
           wordCorrectCount: head.wordCorrectCount,
           wordIncorrectCount: head.wordIncorrectCount,
           totalScore: head.totalScore
@@ -22,22 +27,76 @@ class LearningRoute extends Component {
       })
   }
 
+  handleSubmitAnswer(event) {
+    event.preventDefault()
+    console.log(this.state.guess)
+    LanguageService.postGuess(this.state.guess)
+      .then(head => {
+        if(head.totalScore === this.state.totalScore) {
+          this.setState({
+            nextWord: head.nextWord,
+            answer: head.answer,
+            wordCorrectCount: head.wordCorrectCount,
+            wordIncorrectCount: head.wordIncorrectCount,
+            totalScore: head.totalScore,
+            correctGuess: false
+          })
+        }
+
+        else if (head.totalScore > this.state.totalScore) {
+          this.setState({
+            nextWord: head.nextWord,
+            answer: head.answer,
+            wordCorrectCount: head.wordCorrectCount,
+            wordIncorrectCount: head.wordIncorrectCount,
+            totalScore: head.totalScore,
+            correctGuess: true
+          })
+        }
+
+      })
+  }
+
+  handleChangeAnswer = (guess) => {
+    this.setState({ guess: guess.target.value })
+  }
+
   render() {
+    let correctGuess = this.state.correctGuess
     return (
       <section>
-        <h2>{'Translate the word:'}</h2>
-        <form className='main-form'>
+        <h2>
+          {correctGuess === 0 && 'Translate the word:'}
+          {correctGuess === true && 'You were correct! :D'}
+          {correctGuess === false && 'Good try, but not quite right :('}
+        </h2>
+
+        <span>{this.state.nextWord}</span>
+
+        <form className='main_form' onSubmit={(event) => this.handleSubmitAnswer(event)}>
           <label htmlFor='learn-guess-input'>What's the translation for this word?</label>
-          <input type='text' id='learn-guess-input' required></input>
-          <button type='submit'>Submit your answer</button>
+          <input 
+            type='text' 
+            id='learn-guess-input' 
+            className='guess' 
+            onChange={(event) => this.handleChangeAnswer(event)}
+            required></input>
+          <button type='submit'>
+            {this.state.nextWord === this.state.lastWord && 'Submit your answer'}
+            {this.state.nextWord !== this.state.lastWord && 'Try another word!'}
+          </button>
         </form>
 
-        <p>{`Your total score is: ${this.state.totalScore}`}</p>
+        <div className='DisplayScore'>
+          <p>{`Your total score is: ${this.state.totalScore}`}</p>
+        </div>
+
+        <div className='DisplayFeedback'>
+          <p>{this.state.answer && `The correct translation for ${this.state.lastWord} was ${this.state.answer} and you chose ${this.state.guess}!`}</p>
+        </div>
 
         <p>{`You have answered this word correctly ${this.state.wordCorrectCount} times.`}</p>
         <p>{`You have answered this word incorrectly ${this.state.wordIncorrectCount} times.`}</p>
-
-        <span>Testnextword</span>
       </section>
     );
   }
--- src/services/language-service.js
+++ src/services/language-service.js
@@ -1,7 +1,7 @@
 import config from '../config'
 import TokenService from './token-service'
 
-const LanguageService = {
+export default {
   getAll() {
     return fetch (`${config.API_ENDPOINT}/language`, {
       headers: {
@@ -26,7 +26,21 @@ const LanguageService = {
         ? res.json().then(e => Promise.reject(e))
         : res.json()
     })
+  },
+
+  postGuess(guess) {
+    return fetch(`${config.API_ENDPOINT}/language/guess`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify({guess})
+    })
+    .then(res => {
+      return (!res.ok)
+        ? res.json().then(e => Promise.reject(e))
+        : res.json()
+    })
   }
 }
-
-export default LanguageService
